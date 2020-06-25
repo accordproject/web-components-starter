8f98d530b007fe442539f8c1834ae2a8242f1ebe
Diana Lease <dianarlease@gmail.com>
2020-06-24 20:14:24 -0400

    feat(*): create example React app that uses ConcertoForm
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

---
+++ src/ConcertoEditor.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CodeEditor from './CodeEditor';
+import { CONCERTO_FORMAT, CONCERTO_THEME } from './concerto';
+
+/**
+ * A code editing component for Concerto files
+ * @param {*} props the properties for the component
+ */
+function ConcertoEditor(props) {
+  return (<CodeEditor
+    handleSubmit={props.handleSubmit}
+    textValue={props.textValue}
+    languageId='concerto'
+    languageFormat={CONCERTO_FORMAT}
+    themeId='concerto-light'
+    theme={CONCERTO_THEME}
+    debounceInterval={1000}
+  />);
+}
+
+ConcertoEditor.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  textValue: PropTypes.string.isRequired,
+};
+
+export default ConcertoEditor;
